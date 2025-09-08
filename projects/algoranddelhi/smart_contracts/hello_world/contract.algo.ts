e5bbff351135e1a6339361da7886b4ffe9e1a95e
Adarsh-pratap-singh09 <adarshpratap3425@gmail.com>
2025-09-08 22:22:31 +0530

    done

--- projects/algoranddelhi/smart_contracts/hello_world/contract.algo.ts
+++ projects/algoranddelhi/smart_contracts/hello_world/contract.algo.ts
@@ -1,11 +1,11 @@
-import { Contract,GlobalState } from '@algorandfoundation/algorand-typescript'
+import { Contract,GlobalState, uint64 } from '@algorandfoundation/algorand-typescript'
 
 export class WaterLogger extends Contract {
   // Store total water intake (in ml) as global state
-  totalIntake = GlobalState<number>({ key: "totalIntake", initialValue: 0 })
+  totalIntake = GlobalState<uint64>({ key: "totalIntake", initialValue: 0 })
 
   // Function to log water intake
-  logIntake(amount: number): number {
+  logIntake(amount: uint64): uint64 {
     // Increase total intake
     this.totalIntake.value = this.totalIntake.value + amount
 
--- projects/algoranddelhi/smart_contracts/hello_world/deploy-config.ts
+++ projects/algoranddelhi/smart_contracts/hello_world/deploy-config.ts
@@ -1,5 +1,5 @@
 import { AlgorandClient } from '@algorandfoundation/algokit-utils'
-import { HelloWorldFactory } from '../artifacts/hello_world/HelloWorldClient'
+import { WaterLoggerFactory } from '../artifacts/hello_world/WaterLoggerClient'
 
 // Below is a showcase of various deployment options you can use in TypeScript Client
 export async function deploy() {
@@ -8,7 +8,7 @@ export async function deploy() {
   const algorand = AlgorandClient.fromEnvironment()
   const deployer = await algorand.account.fromEnvironment('DEPLOYER')
 
-  const factory = algorand.client.getTypedAppFactory(HelloWorldFactory, {
+  const factory = algorand.client.getTypedAppFactory(WaterLoggerFactory, {
     defaultSender: deployer.addr,
   })
 
@@ -23,11 +23,12 @@ export async function deploy() {
     })
   }
 
-  const method = 'hello'  
-  const response = await appClient.send.hello({
-    args: { name: 'world' },
-  })
+ const method = 'hello'  
+ const response = await appClient.send.logIntake({
+  args: {
+    amount: 500 }
+ })
   console.log(
-    `Called ${method} on ${appClient.appClient.appName} (${appClient.appClient.appId}) with name = world, received: ${response.return}`,
+    `Called ${method} on ${appClient.appClient.appName} (${appClient.appClient.appId})  ${response.return}`,
   )
 }
